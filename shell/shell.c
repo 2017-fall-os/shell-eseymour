7563afcbe56a3fe820b594f8e433286c96f7be63
Edward Seymour <dev@edwardseymour.me>
2017-09-21 19:30:37 -0600

    Add PATH searching

--- shell/shell.c
+++ shell/shell.c
@@ -16,6 +16,23 @@ char *nullTok[] = {(char *) 0};
 
 char *prompt = "$ ";
 
+char ** getPathTok(char *envp[]) {
+  for (; *envp != 0; envp++) {
+    char **varTok = mytok(*envp, '=');
+
+    if (stringcmp(varTok[0], "PATH") == 0) {
+      char **pathTok = mytok(varTok[1], ':');
+      freetok(varTok);
+      return pathTok;
+    }
+
+    freetok(varTok);
+  }
+
+  write(STDERR_FILENO, "PATH enviornment variable not defined\n", 38);
+  exit(EXIT_FAILURE);
+}
+
 void shellLoop(char *envp[]) {
   for (int keepLooping = 1; keepLooping;) {
     char *line = readline(prompt);
@@ -45,6 +62,19 @@ int shellExec(char **tokens, char *envp[]) {
       exit(EXIT_FAILURE);
     } else if (fpid == 0) {
       // Child process
+      size_t commandLen = toklen(tokens[0], '\0');
+      for (char **pathTok = getPathTok(envp); *pathTok != 0; pathTok++) {
+        char *curPath = *pathTok;
+        size_t curPathLen = toklen(curPath, '\0');
+        curPath = realloc(curPath, curPathLen + commandLen + 2);
+
+        curPath[curPathLen] = '/';
+        for (int i = 0; i <= commandLen; i++) {
+          curPath[curPathLen + i + 1] = tokens[0][i];
+        }
+
+        execve(curPath, tokens, envp);
+      }
       execve(tokens[0], tokens, envp);
       write(STDERR_FILENO, "command not found\n", 18);
       exit(EXIT_FAILURE);
