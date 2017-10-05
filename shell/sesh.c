a94d5094ede5e8df3a070dcdf68fe129d2e606d8
Edward Seymour <dev@edwardseymour.me>
2017-10-05 15:46:03 -0600

    Pipe readline output to /dev/null if not tty

--- shell/sesh.c
+++ shell/sesh.c
@@ -1,5 +1,19 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <readline/readline.h>
+
 #include "shell.h"
 
 int main(int argc, char *argv[], char *envp[]) {
+  // Initialize
+  if (!isatty(STDIN_FILENO)) {
+    // Stop readline from echoing to stdout if not run interactively.
+    // Pretty hacky, should implement own line reading functionality.
+    int devNull = open("/dev/null", O_WRONLY);
+    rl_outstream = fdopen(devNull, "w");
+  }
+
   shellLoop(envp);
 }
