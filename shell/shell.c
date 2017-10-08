aaabd823f95ff0b56708be4e6da7a6dcfa587ad0
Edward Seymour <dev@edwardseymour.me>
2017-10-08 17:55:22 -0600

    Add piping of commands

--- shell/shell.c
+++ shell/shell.c
@@ -46,7 +46,7 @@ void shellLoop(char *envp[]) {
       break;
     }
 
-    char **tokens = tokenize(line, ' ');
+    char **tokens = tokenize(line, '|');
     shellExec(tokens, envp);
 
     tokenFree(tokens);
@@ -54,47 +54,106 @@ void shellLoop(char *envp[]) {
   }
 }
 
-void shellExec(char **tokens, char *envp[]) {
-  if (tokenCmp(tokens, nullTok) != 0) {
-    // Check if builtin
-    BuiltinFunc builtin = getBuiltinFunc(tokens[0]);
-    if (builtin != NULL) {
-      // Excecute builtin function and return
-      builtin(tokenLen(tokens), tokens, envp);
+void shellExec(char **commands, char *envp[]) {
+  int numCommands = tokenLen(commands);
 
-      return;
+  if (numCommands <= 1 && strNumTokens(commands[0], ' ') == 0) {
+    // No command typed in.
+    return;
+  }
+
+  int infile = -1;
+  for (int i = 0; i < numCommands - 1; i++) {
+    // On left side of a pipe, open a pipe
+    int pipedes[2];
+    pipe(pipedes);
+
+    char **args = tokenize(commands[i], ' ');
+    shellCommandExec(args, envp, infile, pipedes[1]);
+    tokenFree(args);
+    
+    // Close left side of pipe and last input file
+    close(pipedes[1]);
+    if (infile != -1) close(infile);
+
+    infile = pipedes[0];
+  }
+
+  // Excecute last command without opening a pipe
+  char **args = tokenize(commands[numCommands - 1], ' ');
+  pid_t lastPid = shellCommandExec(args, envp, infile, -1);
+  tokenFree(args);
+
+  if (infile != -1) close(infile);
+
+  int exitStatus;
+  if (lastPid > 0) {
+    // Last command was an external program
+    int status;
+    waitpid(lastPid, &status, 0);
+    if (WIFEXITED(status)) {
+      exitStatus = WEXITSTATUS(status);
     }
-
-    pid_t fpid = fork();
-    if (fpid < 0) {
-      // Failed to fork
-      write(STDERR_FILENO, "Failed to fork.\n", 16);
-      exit(EXIT_FAILURE);
-    } else if (fpid == 0) {
-      // Child process
-      size_t commandLen = stringlen(tokens[0]);
-      for (char **pathTok = getPathTok(envp); *pathTok != 0; pathTok++) {
-        char *curPath = *pathTok;
-        size_t curPathLen = stringlen(curPath);
-        curPath = realloc(curPath, curPathLen + commandLen + 2);
-
-        curPath[curPathLen] = '/';
-        for (int i = 0; i <= commandLen; i++) {
-          curPath[curPathLen + i + 1] = tokens[0][i];
-        }
-
-        execve(curPath, tokens, envp);
-      }
-      execve(tokens[0], tokens, envp);
-      write(STDERR_FILENO, "command not found\n", 18);
-      exit(EXIT_SUCCESS);
-    } else {
-      // Parent process
-      int status;
-      wait(&status);
-      if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
-        fprintf(stderr, "program terminated with exit code: %d\n", WEXITSTATUS(status));
-      }
+    else {
+      // Did not exit, use a different exit status
+      exitStatus = -1;
     }
   }
+  else {
+    // Last command was a builtin
+    exitStatus = -lastPid;
+  }
+
+  if (exitStatus != 0) {
+    fprintf(stderr, "command terminated with exit code: %d\n", exitStatus);
+  }
+}
+
+pid_t shellCommandExec(char **args, char *envp[], int indes, int outdes) {
+  BuiltinFunc builtin = getBuiltinFunc(args[0]);
+  if (builtin != NULL) {
+    // Is a builtin function, excecute and return negative exit condition
+    return -builtin(tokenLen(args), args, envp);
+  }
+
+  // Not a builtin function
+  pid_t fpid = fork();
+  if (fpid < 0) {
+    // Failed to fork
+    write(STDERR_FILENO, "Failed to fork.\n", 16);
+    exit(EXIT_FAILURE);
+  }
+  else if (fpid == 0) {
+    // Child process
+
+    // Change descripors if necessary
+    if (indes != -1) {
+      dup2(indes, STDIN_FILENO);
+      close(indes);
+    }
+    if (outdes != -1) {
+      dup2(outdes, STDOUT_FILENO);
+      close(outdes);
+    }
+    
+    // Find command in path
+    size_t commandLen = stringlen(args[0]);
+    for (char **pathTok = getPathTok(envp); *pathTok != 0; pathTok++) {
+      char *curPath = *pathTok;
+      size_t curPathLen = stringlen(curPath);
+      curPath = realloc(curPath, curPathLen + commandLen + 2);
+
+      curPath[curPathLen] = '/';
+      for (int i = 0; i <= commandLen; i++) {
+        curPath[curPathLen + i + 1] = args[0][i];
+      }
+
+      execve(curPath, args, envp);
+    }
+    execve(args[0], args, envp);
+    write(STDERR_FILENO, "command not found\n", 18);
+    exit(EXIT_SUCCESS);
+  }
+  
+  return fpid;
 }
--- shell/shell.h
+++ shell/shell.h
@@ -1,7 +1,10 @@
 #ifndef SHELLLAB_SHELL_H
 #define SHELLLAB_SHELL_H
 
+#include <sys/types.h>
+
 void shellLoop(char *envp[]);
-void shellExec(char **tokens, char *envp[]);
+void shellExec(char **commands, char *envp[]);
+pid_t shellCommandExec(char **args, char *envp[], int indes, int outdes);
 
 #endif // SHELLLAB_SHELL_H
